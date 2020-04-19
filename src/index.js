d2f397a56b58f5d29683bc72590b3cec03c53576
thalesleite <thalesaleite@gmail.com>
2020-04-19 12:01:22 +0100

    added player and config the sprites

--- src/index.js
+++ src/index.js
@@ -32,17 +32,42 @@ function preload () {
 }
 
 function create () {
-    const platforms = this.physics.add.staticGroup();
+    let platforms, player;
 
     //this.add.image(0, 0, 'sky').setOrigin(0, 0);
     this.add.image(400, 300, 'sky');
 
+    platforms = this.physics.add.staticGroup()
     // ground of the game
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
     // platforms
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+    
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update () {
