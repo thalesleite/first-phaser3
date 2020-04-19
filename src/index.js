5d4c7293aa6cc3ef34c8c6de5f1d1281b80017de
thalesleite <thalesaleite@gmail.com>
2020-04-19 12:39:02 +0100

    added bombs and implemented gameover logic

--- src/index.js
+++ src/index.js
@@ -17,7 +17,13 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-let player, cursors;
+let player, 
+    cursors,
+    stars,
+    bombs,
+    scoreText,
+    score = 0,
+    gameOver = false;
 
 function preload () {
     this.load.image('sky', '../assets/sky.png');
@@ -69,12 +75,34 @@ function create () {
         repeat: -1
     });
 
-    this.physics.add.collider(player, platforms);
-
     cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(child => child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8) ));
+
+    bombs = this.physics.add.group();
+
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update () {
+    if (gameOver) {
+        return;
+    }
+
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
         player.anims.play('left', true);
@@ -91,4 +119,34 @@ function update () {
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-330);
     }
+}
+
+function collectStar (player, star){
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate(function (child) {
+            child.enableBody(true, child.x, 0, true, true);
+        });
+
+        const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        const bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+}
+
+function hitBomb (player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+
+    gameOver = true;
 }
